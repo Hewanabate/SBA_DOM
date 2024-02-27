f13b6b4c251db3cfcb9debcac44a31b8f1668a6d
HAA <hewanabate2017@gmail.com>
2024-02-27 17:45:07 -0600

    Final

--- script.js
+++ script.js
@@ -11,18 +11,91 @@ var menuNavbar = [
 const menuBar = document.getElementById('navbar');
 
 // iterate over a collection of elements
-menuNavbar.forEach((menu)=>{
-//create element
-    const menuItem = document.createElement('a');
-    menuItem.textContent = menu.text;
-    menuItem.href = menu.href;
-    navbar.appendChild(menuItem);
+menuNavbar.forEach((menu) => {
+  //create element
+  const menuItem = document.createElement('a');
+  menuItem.textContent = menu.text;
+  menuItem.href = menu.href;
+  //append child
+  menuBar.appendChild(menuItem);
 });
 
 // select element using querySelector
-const bar = document.querySelector('div');
-// using innerHTML
-divbar.innerHTML = '<h1>WOW Beauty</h1>';
-divbar.style.color ='white'
-  
+const divbar = document.querySelector('div');
 
+// using innerHTML
+divbar.innerHTML = '<h1><i>WOW Beauty</i></h1>';
+divbar.style.color = 'white';
+
+const para = document.getElementById('app');
+para.innerHTML =
+  ' <p><i><u>Vist US</u></i></p><p><i>1234 DownTown</i></p><p><i>Dallas, TX 73475</i></p><p><i>Tel: 460-987-6789</i></p><p><i>wowbeauty@.co</i></p><br><p><i><u>Open Hours</u></i></p><p><i>Monday - Friday 9am-6pm | Saturday 9am - 4pm</i></p>';
+para.style.color = 'white';
+
+// use firstElementChild
+const town = document.querySelector('p');
+town.firstElementChild.style.color="gold";
+
+// addEventlistener on submit 
+const myForm = document.querySelector("form");
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // alert the user
+  alert(`You submitted: 
+  ${myForm.querySelector("input").value}`);
+
+  //reset the form
+  myForm.reset();
+
+});
+
+
+const form = document.querySelector("form");
+const email = form.elements["email"];
+const password = form.elements["password"];
+
+form.addEventListener("submit", validate);
+
+// The validation function.
+
+function validate(event) {
+// validate email
+  const emailVal = validateEmail();
+  if (emailVal === false) {
+    event.returnValue = false;
+    return false;
+  }
+//validate password
+  const passwordVal = validatePassword();
+  if (passwordVal === false) {
+    event.returnValue = false;
+    return false;
+  }
+
+  return true;
+}
+
+//Select and cache the all of the <a> elements inside of menuNavbar 
+
+const menuItem1 = navbar.querySelectorAll('a');
+
+navbar.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  if (!event.target.matches('a')) {
+    return;
+  }
+
+  console.log(event.target.textContent);
+
+  if (event.target.classList.contains('active')) {
+    event.target.classList.remove('active');
+  } else {
+    //The event listener should remove the active class from each other <a> element in navMenubar - whether the active class exists or not.
+    menuItem1.forEach(function (link) {
+      link.classList.remove('active');
+    });
+    event.target.classList.add('active');
+  }
+}); 
